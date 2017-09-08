c02b1bc125fc465d3cb13a399ee717ef5aa0bdef
Marc <RhinoDevel@users.noreply.github.com>
2017-09-08 11:52:19 +0200

    Comments about and test input.

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 # RhinoDevel, Marcel Timm, 2017.09.07
 
+# Tested with:
+#
+# - Commodore/CBM 3001 Series Computer 3032, PET 2001-32N C with Basic 2.0
+
+# - PET's user port pins interpreted as outputs have LOW level, if PET is powered off.
+# - They are "set" to HIGH level (interpreted as outputs) during PET booting up. 
+# - Initially, the I/O user port pins 0-7 are configured as inputs: PEEK(59459) => 0
+# - Such a pin can be configured as output via poking to 59459. E.g. for pin 1: POKE 59459,(PEEK(59459) OR 2) => LOW level (maybe not always!).
+# - Output level can be set by poking to 59471. E.g. for pin 1: POKE 59471,(PEEK(59471) OR 2) => HIGH level.
+
 import RPi.GPIO as GPIO
 import time
 
 pin_mode = GPIO.BCM # GPIO.BOARD
 pin_0 = 4 # BCM
+pin_1 = 17 # BCM
 
 def setup_pins():
     GPIO.setup(pin_0, GPIO.OUT)
+    GPIO.setup(pin_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 def setup():
     GPIO.setwarnings(False)
@@ -18,11 +30,26 @@ def set_output(pin_nr, val):
     print('Setting pin '+str(pin_nr)+' to '+str(val)+'..')
     GPIO.output(pin_nr, val)
 
+def get_input(pin_nr):
+    val = GPIO.input(pin_nr)
+    print('Input at pin '+str(pin_nr)+' is '+str(val)+'.')
+    return val
+
 def main():
     b = 137
     val = GPIO.LOW
 
     setup()
+        
+    #time.sleep(10)
+
+    get_input(pin_1)
+    
+    GPIO.wait_for_edge(pin_1, GPIO.BOTH)
+    time.sleep(1)
+    get_input(pin_1)
+
+    return
 
     print('Initialising..')
     set_output(pin_0, GPIO.LOW)
