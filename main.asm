c0bb5fa021182b87252c6bfed1e0747b3266c4ca
Marc <RhinoDevel@users.noreply.github.com>
2017-09-13 20:39:40 +0200

    Almost finished assembler port of basic application written with CBM prg Studio.

---
+++ main.asm
@@ -0,0 +1,182 @@
+
+; 2017.09.13
+;
+; marcel timm, rhinodevel
+
+; cbm pet
+
+*=634 ;tape buf. #1 and #2 used (192+192 bytes)
+
+; system sub routines
+
+clrscr   = $e236          ;$e246
+crlf     = $c9d2          ;$c9e2
+wrt      = $ffd2
+strout   = $ca27          ;$ca1c
+
+; "constants"
+
+char_0   = $30
+char_a   = $41
+time     = 514          ;low byte of time
+di       = 59459          ;data direction reg.
+io       = 59471          ;i/o port
+
+de       = 1           ;1/60secs.bit read delay
+
+; *** main ***
+
+         cld           ;probably not needed
+
+         jsr clrscr
+
+         ldy #>setouth
+         lda #<setouth
+         jsr strout
+         jsr crlf
+         jsr togout
+
+         ldy #>enableo
+         lda #<enableo
+         jsr strout
+         jsr crlf
+         lda di
+         ora #2
+         sta di
+
+         ldy #>setoutl
+         lda #<setoutl
+         jsr strout
+         jsr crlf
+         jsr togout
+
+         jsr readbyte
+         sta adl
+         jsr readbyte
+         sta adh
+         ldy #>starta
+         lda #<starta
+         jsr strout
+         lda adh
+         jsr printby
+         lda adl
+         jsr printby
+         jsr crlf
+
+         jsr readbyte
+         sta lel
+         jsr readbyte
+         sta leh
+         ldy #>bycount
+         lda #<bycount
+         jsr strout
+         lda leh
+         jsr printby
+         lda lel
+         jsr printby
+         jsr crlf
+
+         ldy #>readpl
+         lda #<readpl
+         jsr strout
+                       ;todo: read payload, etc.
+
+         rts
+
+; *** toggle output ***
+
+togout   lda #1
+         sec
+         sbc o
+         sta o
+         cmp 0
+         beq togoutl
+         lda io        ;toggle output to high
+         ora #2
+         sta io
+         rts
+togoutl  lda io        ;toggle output to low
+         and #253
+         sta io
+         rts
+
+; *** wait 1/60 secs.in constant de ***
+
+waitde   sei           ;no update during read
+         lda time      ;read low byte of time
+         cli
+         clc
+         adc #de       ;calculate resume time
+delay    cmp time      ;loop, untile resume
+         bne delay     ;time is reached
+         rts
+
+;*** read a byte into accumulator ***
+
+readbyte ldy #0        ;byte buffer during read
+         ldx #1        ;to hold 2^exp
+readloop jsr waitde
+         lda io
+         and #1
+         beq readnext  ;bit read is zero
+         stx bi        ;bit read is one, add to byte (buffer)
+         tya           ;get current byte buffer content
+         ora bi        ;"add" current bit read
+         tay           ;save into byte buffer
+readnext txa           ;get next 2^exp
+         asl
+         tax
+         jsr togout    ;acknowledge
+         cpx #0        ; last bit read?
+         bne readloop
+         tya           ;get byte read into accumulator
+         rts
+
+; *** print "hexadigit" (hex.0-f) stored in accumulator ***
+
+printhd  and #$0f      ;ignore left 4 bits
+         cmp #$0a
+         bcc printd
+         clc           ;more or equal $0a - a to f
+         adc #char_a-$0a
+         bcc print
+printd   clc           ;less than $0a - 0 to 9
+         adc #char_0
+print    jsr wrt
+         rts
+
+; *** print byte in accumulator as hexadecimal value ***
+
+printby  ldx #4
+         tay
+prbloop  lsr a
+         dex
+         bne prbloop
+         jsr printhd
+         tya
+         jsr printhd
+         rts
+
+; variables
+
+o        byte 0 ;output val.(hard-coded)
+bi       byte 0 ;byte representation of current bit read ;todo: use zero page.
+adl      byte 0 ;where to store bytes
+adh      byte 0 ;
+lel      byte 0 ;count of payload bytes
+leh      byte 0 ;
+
+; data
+
+setouth  text "setting out val. to high.."
+delim1   byte 0
+enableo  text "enabling output.."
+delim2   byte 0
+setoutl  text "setting out val. to low.."
+delim3   byte 0
+starta   text "start address: "
+delim4   byte 0
+bycount  text "byte count: "
+delim5   byte 0
+readpl   text "reading payload.."
+delim6   byte 0
