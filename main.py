ad7abb8ad6270d5c89291b5dbbc88a14953458b7
Marc <RhinoDevel@users.noreply.github.com>
2017-09-13 14:09:44 +0200

    Improve sending (reduce wait time) and output.

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ pin_mode = GPIO.BCM # GPIO.BOARD
 pin_0 = 4 # BCM
 pin_1 = 17 # BCM
 
-edge_wait_seconds = 0.2 # Check by sending a zero.
+edge_wait_seconds = 0.001 # Use slightly higher delay on PET.
 
 def setup_pins():
     GPIO.setup(pin_0, GPIO.OUT)
@@ -31,17 +31,23 @@ def setup():
     setup_pins()
 
 def set_output(pin_nr, val):
-    print('Setting pin '+str(pin_nr)+' to '+str(val)+'..')
+    #print('set_output : '+'Setting pin '+str(pin_nr)+' to '+str(val)+'..')
     GPIO.output(pin_nr, val)
 
 def get_input(pin_nr):
     val = GPIO.input(pin_nr)
-    print('Input at pin '+str(pin_nr)+' is '+str(val)+'.')
+    #print('get_input : '+'Input at pin '+str(pin_nr)+' is '+str(val)+'.')
     return val
 
 def send_byte(b):
     i = 0
     val = GPIO.LOW
+    next_edge = GPIO.RISING
+
+    # Should be an assert (debugging):
+    #
+    if get_input(pin_1) is GPIO.HIGH:
+        raise Exception('send_byte : Error: Input pin must be set to low!')
 
     for i in range(0,8):
         if (b>>i)&1 == 1:
@@ -49,8 +55,14 @@ def send_byte(b):
         else:
             val = GPIO.LOW
         set_output(pin_0, val)
+
         time.sleep(edge_wait_seconds) # To avoid detecting false edge.
-        GPIO.wait_for_edge(pin_1, GPIO.BOTH)
+
+        GPIO.wait_for_edge(pin_1, next_edge)
+        if next_edge is GPIO.FALLING:
+           next_edge = GPIO.RISING
+        else:
+           next_edge = GPIO.FALLING
 
 def main():
     #start_addr = 826 # ROM v2 and v3 tape #2 buffer.
@@ -73,34 +85,41 @@ def main():
     del payload[0]
     del payload[0]
 
+    payload_len = len(payload)
     b = -1
     l = -1
     h = -1
     t0 = None
+    t_end = None
 
     setup()
-        
-    get_input(pin_1)
+       
+    if get_input(pin_1) is not GPIO.HIGH:
+        print('Error: Input must be set to HIGH! Exiting..')
+        return
 
     print('Waiting for start signal..')
-    GPIO.wait_for_edge(pin_1, GPIO.BOTH)
+    GPIO.wait_for_edge(pin_1, GPIO.FALLING)
 
     t0 = time.time()
 
-    print('Starting transfer..')
+    print('Starting transfer of 2+2+'+str(payload_len)+' bytes..')
     h = start_addr//256 # ("Python-style") integer division.
     l = start_addr-256*h
     send_byte(l)
     send_byte(h)
-    h = len(payload)//256
-    l = len(payload)-256*h
+    h = payload_len//256
+    l = payload_len-256*h
     send_byte(l)
     send_byte(h)
     for b in payload:
         send_byte(b)
     print('Transfer done.')
 
-    print('Elapsed time: '+str(time.time()-t0))
+    t_end = time.time()-t0
+
+    print('Elapsed seconds: '+str(t_end))
+    print('Bytes per second: '+str((2+2+payload_len)/t_end))
 
     print('Cleaning up..')
     GPIO.cleanup()
