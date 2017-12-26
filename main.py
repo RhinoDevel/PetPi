624138edbb686780974617d35fa19d5074db0f72
Marc <RhinoDevel@users.noreply.github.com>
2017-12-26 21:23:10 +0100

    BUGFIX: Sporadic transfer errors; Speed-up; Simplify code; Improve messages.

--- main.py
+++ main.py
@@ -20,13 +20,8 @@ pin_0_data_to_pet = 4 # BCM
 pin_1_read_ack_from_pet = 17 # BCM
 pin_2_wrt_rdy_to_pet = 27 # BCM
 
-next_read_ack_edge = GPIO.RISING # See start signal.
-
-timeout_wait_for_start_signal_ms = 60000
-timeout_wait_for_read_ack_from_pet_ms = 20000
-
-immediate_err_count = 0
-immediate_err_seconds = 0.1
+pet_max_rise_seconds = 2.0*1e-6
+pet_max_fall_seconds = 50.0*1e-9
 
 def setup_pins():
     GPIO.setup(pin_0_data_to_pet, GPIO.OUT, initial=GPIO.LOW) # DATA to PET (init. val. shouldn't matter).
@@ -56,11 +51,13 @@ def cleanup():
     GPIO.cleanup()
 
 def send_bit(b):
-    global next_read_ack_edge
-    global immediate_err_count
-
+    expected_read_ack = get_input(pin_2_wrt_rdy_to_pet)
+    wrt_rdy = not expected_read_ack # Just toggles.
+    pet_max_seconds = pet_max_rise_seconds if expected_read_ack is GPIO.HIGH else pet_max_fall_seconds
     val = GPIO.LOW
 
+    # Should be an assert (debugging):
+    #
     if b not in (0, 1):
         cleanup()
         raise Exception('send_bit : Error: Invalid value given!')
@@ -69,44 +66,15 @@ def send_bit(b):
         val = GPIO.HIGH
 
     set_output(pin_0_data_to_pet, val) # DATA to PET.
+    set_output(pin_2_wrt_rdy_to_pet, wrt_rdy) # WRITE READY to PET.
 
-    # WRITE READY to PET:
+    # TODO: Find a way to increase time precision to also increase speed:
     #
-    # - This is OK, see: https://sourceforge.net/p/raspberry-gpio-python/wiki/Outputs/
-    #
-    set_output(pin_2_wrt_rdy_to_pet, not get_input(pin_2_wrt_rdy_to_pet))
-
-    print('send_bit : DATA = ' + str(get_input(pin_0_data_to_pet)) + ', WRT_RDY = ' + str(get_input(pin_2_wrt_rdy_to_pet)) + ' READ_ACK = ' + str(get_input(pin_1_read_ack_from_pet)) + ', waiting for READ_ACK = ' + str(next_read_ack_edge)) 
-
-    # Waiting for READ ACK from PET:
-    #
-    if GPIO.wait_for_edge(
-            pin_1_read_ack_from_pet,
-            next_read_ack_edge,
-            timeout=timeout_wait_for_read_ack_from_pet_ms) is None:
-        cleanup()
-        raise Exception('send_bit : Error: READ ACK timeout happened!')
-
-    # TODO: Debug/workaround code:
-    #
-    debu = GPIO.HIGH
-    if next_read_ack_edge is GPIO.RISING:
-        debu = GPIO.LOW
-    #
-    if get_input(pin_1_read_ack_from_pet) is debu:
-        cleanup()
-        raise Exception('*** IMMEDIATE ERROR (did not try to wait as workaround) ***')
-        #immediate_err_count = immediate_err_count+1
-        #print('*** send_bit : Warning: Immediate error (waiting ' + str(immediate_err_seconds) + ' seconds).. ***')
-        #time.sleep(immediate_err_seconds)
-        #if get_input(pin_1_read_ack_from_pet) is debu:
-        #    cleanup()
-        #    raise Exception('*** IMMEDIATE ERROR (waiting did not help) ***')
-
-    if next_read_ack_edge is GPIO.FALLING:
-        next_read_ack_edge = GPIO.RISING
-    else:
-        next_read_ack_edge = GPIO.FALLING
+    while get_input(pin_1_read_ack_from_pet) is not expected_read_ack:
+        stop_time = time.time()+pet_max_seconds        
+        while time.time()<stop_time:
+            pass
+        
 
 def send_byte(b):
     i = 0
@@ -122,12 +90,6 @@ def send_byte(b):
     if get_input(pin_2_wrt_rdy_to_pet) is not GPIO.HIGH:
         cleanup()
         raise Exception('send_byte : Error: WRITE READY output must be set to high!')
-
-    # Should be an assert (debugging):
-    #
-    if next_read_ack_edge is not GPIO.RISING:
-        cleanup()
-        raise Exception('send_byte : Error: Expected next READ ACK edge must be set to rising!')
 
     for i in range(0,8):
         send_bit(b>>i&1)
@@ -153,7 +115,6 @@ def main_nocatch():
     start_addr = payload[0]+256*payload[1]
     del payload[0]
     del payload[0]
-
     payload_len = len(payload)
     b = -1
     l = -1
@@ -164,20 +125,20 @@ def main_nocatch():
     setup()
 
     if get_input(pin_1_read_ack_from_pet) is not GPIO.HIGH:
-        print('Error: Input must be set to HIGH! Exiting..')
+        print('Input must be set to HIGH. Exiting..')
         cleanup()
         return
 
+    # This should be OK, because PET's max. falling time is below 50ns:
+    #
     print('Waiting for start signal..')
     if GPIO.wait_for_edge(
             pin_1_read_ack_from_pet,
             GPIO.FALLING,
-            timeout=timeout_wait_for_start_signal_ms) is None:
+            timeout=60000) is None:
         cleanup()
 	print('Start signal timeout happened.')
         return
-
-    t0 = time.time()
 
     print('Starting transfer of 2+2+'+str(payload_len)+' bytes..')
     h = start_addr//256 # ("Python-style") integer division.
@@ -192,18 +153,21 @@ def main_nocatch():
     send_byte(l)
     print('Sending payload length high byte: '+str(h)+'..')    
     send_byte(h)
-    for i in range(len(payload)):
+
+    t0 = time.time()
+
+    print('Sending payload ('+str(payload_len)+' bytes)..')
+    for i in range(payload_len):
         send_byte(payload[i])
     print('Transfer done.')
 
     t_end = time.time()-t0
 
-    print('Elapsed seconds: '+str(t_end))
-    print('Bytes per second: '+str((2+2+payload_len)/t_end))
+    print('Elapsed seconds for payload transfer: '+str(t_end))
+    print('Bytes per second for payload transfer: '+str((2+2+payload_len)/t_end))
 
     cleanup()
 
-    print('Immediate error count: '+str(immediate_err_count))
     print('Done.')
 
 def main():
@@ -232,6 +196,3 @@ main()
 #print('Key for cleanup and exit..')
 #raw_input()
 #cleanup()
-
-
-
