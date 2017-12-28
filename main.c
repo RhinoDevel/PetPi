81a38d500b3eb0f6f2d09b781188c69ab6790457
Marc <RhinoDevel@users.noreply.github.com>
2017-12-28 22:56:39 +0100

    C GPIO test code (input). - works!

--- main.c
+++ main.c
@@ -5,8 +5,34 @@
 
 int main()
 {
-    gpio_init();
-    printf("Hello!");
+    if(!gpio_init())
+    {
+        printf("Failed!\n");
+        return -1;
+    }
 
+    gpio_set_input_pull_up(24);
+    if(gpio_read(24))
+    {
+        printf("UP OK\n");
+    }
+    else
+    {
+        printf("UP FAILED\n");
+    }
+
+    gpio_set_input_pull_down(24);
+    if(gpio_read(24))
+    {
+        printf("DOWN FAILED\n");
+    }
+    else
+    {
+        printf("DOWN OK\n");
+    }
+
+    //gpio_set_output
+
+    printf("Ok.\n");
     return 0;
 }
