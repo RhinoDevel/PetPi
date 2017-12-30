fa80ac4412d5aefbc169d2c2479db010b0a5ff1f
Marc <RhinoDevel@users.noreply.github.com>
2017-12-30 09:38:59 +0100

    Blinking LED test - works!

--- main.c
+++ main.c
@@ -11,56 +11,13 @@ int main()
         return -1;
     }
 
-    gpio_set_input_pull_up(24);
-    if(gpio_read(24))
-    {
-        printf("UP OK\n");
-    }
-    else
-    {
-        printf("UP FAILED\n");
-    }
+    gpio_set_output(4, false);
 
-    gpio_set_input_pull_down(24);
-    if(gpio_read(24))
+    while(true)
     {
-        printf("DOWN FAILED\n");
+        gpio_write(4, !gpio_read(4));
+        sleep(1);
     }
-    else
-    {
-        printf("DOWN OK\n");
-    }
-
-    gpio_set_output(24, true);
-    if(gpio_read(24))
-    {
-        printf("OUT HIGH OK\n");
-    }
-    else
-    {
-        printf("OUT HIGH FAILED\n");
-    }
-
-    gpio_write(24, false);
-    if(gpio_read(24))
-    {
-        printf("OUT LOW FAILED\n");
-    }
-    else
-    {
-        printf("OUT LOW OK\n");
-    }
-
-    gpio_write(24, true);
-    if(gpio_read(24))
-    {
-        printf("OUT HIGH 2 OK\n");
-    }
-    else
-    {
-        printf("OUT HIGH 2 FAILED\n");
-    }
-
     printf("Ok.\n");
     return 0;
 }
